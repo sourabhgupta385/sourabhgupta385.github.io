30c03d64bf644eaaad3ac4eff2f223cc567b73c2
Sourabh Gupta <gupta.sourabh385@gmail.com>
2019-10-23 19:56:02 +0530

    first commit

---
+++ precache-manifest.cfef3a7162245633ed634263275fc8b9.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b472ca046955719a351f814dfec3958",
+    "url": "/sourabhgupta/index.html"
+  },
+  {
+    "revision": "95e4fdc4e630263eef7c",
+    "url": "/sourabhgupta/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "78bc6641f1b3f39e9455",
+    "url": "/sourabhgupta/static/css/main.95e4c945.chunk.css"
+  },
+  {
+    "revision": "95e4fdc4e630263eef7c",
+    "url": "/sourabhgupta/static/js/2.d7fb9e27.chunk.js"
+  },
+  {
+    "revision": "78bc6641f1b3f39e9455",
+    "url": "/sourabhgupta/static/js/main.7605d678.chunk.js"
+  },
+  {
+    "revision": "29bff68af9ba5e2697e6",
+    "url": "/sourabhgupta/static/js/runtime-main.89456040.js"
+  },
+  {
+    "revision": "97537c3be7ab6e2f187e9c7c9d386a12",
+    "url": "/sourabhgupta/static/media/amazonaws-logo.97537c3b.png"
+  },
+  {
+    "revision": "96a06b7872f694e844a8dc886827b717",
+    "url": "/sourabhgupta/static/media/css-logo.96a06b78.png"
+  },
+  {
+    "revision": "5345da81e8467003e1ad0d8db99695e9",
+    "url": "/sourabhgupta/static/media/html-logo.5345da81.png"
+  },
+  {
+    "revision": "97a83e97b37952189e980d013d7ddadd",
+    "url": "/sourabhgupta/static/media/isro-logo.97a83e97.png"
+  },
+  {
+    "revision": "09db717dd2b20910f730d69e8d75bf3b",
+    "url": "/sourabhgupta/static/media/java-logo.09db717d.png"
+  },
+  {
+    "revision": "0ea1b87b742cb3578e5f03f3224f3ecd",
+    "url": "/sourabhgupta/static/media/mdl-logo.0ea1b87b.png"
+  },
+  {
+    "revision": "12bdf96307cfcd243e965adf6e5044a8",
+    "url": "/sourabhgupta/static/media/python-logo.12bdf963.png"
+  },
+  {
+    "revision": "41b7c3ab21e831db5989bbde567054d6",
+    "url": "/sourabhgupta/static/media/react-logo.41b7c3ab.png"
+  },
+  {
+    "revision": "7e3add3e50cadd8528640a31389a21c1",
+    "url": "/sourabhgupta/static/media/skitlogo.7e3add3e.jpg"
+  },
+  {
+    "revision": "28a8a434ab6e942b6f3f37d5a70a82a9",
+    "url": "/sourabhgupta/static/media/sourabh.28a8a434.jpg"
+  }
+]);
