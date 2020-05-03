e3e653664de7daa44d35b807ebddc6964ed43b6f
sourabhgupta385 <gupta.sourabh385@gmail.com>
2020-05-03 19:02:24 +0530

    added new projects

---
+++ precache-manifest.cff766acbe80556ac95892c46876e482.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da0e3ceb91c05dece9a70fb14d39fb15",
+    "url": "/sourabhgupta/index.html"
+  },
+  {
+    "revision": "cde845e6f6909de2dbc7",
+    "url": "/sourabhgupta/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "be4b20be05d276f9f57b",
+    "url": "/sourabhgupta/static/css/main.01fb19a4.chunk.css"
+  },
+  {
+    "revision": "cde845e6f6909de2dbc7",
+    "url": "/sourabhgupta/static/js/2.d6be21f1.chunk.js"
+  },
+  {
+    "revision": "be4b20be05d276f9f57b",
+    "url": "/sourabhgupta/static/js/main.ebb3c2b5.chunk.js"
+  },
+  {
+    "revision": "29bff68af9ba5e2697e6",
+    "url": "/sourabhgupta/static/js/runtime-main.89456040.js"
+  },
+  {
+    "revision": "97537c3be7ab6e2f187e9c7c9d386a12",
+    "url": "/sourabhgupta/static/media/amazonaws-logo.97537c3b.png"
+  },
+  {
+    "revision": "96a06b7872f694e844a8dc886827b717",
+    "url": "/sourabhgupta/static/media/css-logo.96a06b78.png"
+  },
+  {
+    "revision": "09b273f822a5469efad2254dac12c08f",
+    "url": "/sourabhgupta/static/media/dashboard.09b273f8.png"
+  },
+  {
+    "revision": "3da1632b253ecc3e508e632590ec694e",
+    "url": "/sourabhgupta/static/media/guess-number.3da1632b.png"
+  },
+  {
+    "revision": "96fa15fc26797a28c43357c62265a99d",
+    "url": "/sourabhgupta/static/media/helm3.96fa15fc.jpg"
+  },
+  {
+    "revision": "5345da81e8467003e1ad0d8db99695e9",
+    "url": "/sourabhgupta/static/media/html-logo.5345da81.png"
+  },
+  {
+    "revision": "97a83e97b37952189e980d013d7ddadd",
+    "url": "/sourabhgupta/static/media/isro-logo.97a83e97.png"
+  },
+  {
+    "revision": "09db717dd2b20910f730d69e8d75bf3b",
+    "url": "/sourabhgupta/static/media/java-logo.09db717d.png"
+  },
+  {
+    "revision": "0ea1b87b742cb3578e5f03f3224f3ecd",
+    "url": "/sourabhgupta/static/media/mdl-logo.0ea1b87b.png"
+  },
+  {
+    "revision": "12bdf96307cfcd243e965adf6e5044a8",
+    "url": "/sourabhgupta/static/media/python-logo.12bdf963.png"
+  },
+  {
+    "revision": "41b7c3ab21e831db5989bbde567054d6",
+    "url": "/sourabhgupta/static/media/react-logo.41b7c3ab.png"
+  },
+  {
+    "revision": "7e3add3e50cadd8528640a31389a21c1",
+    "url": "/sourabhgupta/static/media/skitlogo.7e3add3e.jpg"
+  },
+  {
+    "revision": "619c359dacdf18d1325e828d5eeec5ce",
+    "url": "/sourabhgupta/static/media/sourabh.619c359d.jpg"
+  }
+]);
